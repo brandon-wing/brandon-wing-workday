e1bff04d434f02ee979574878e38a719939d5cab
rudyxwhite <brandonrwing98@gmail.com>
2023-01-19 11:08:09 -0600

    added js to load the data saved by clicking the save button

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,26 @@
 //wrapped the code in a call to jquery
 $(document).ready(function() {
-  console.log("the browser has finished rendering all the elements in the html.");
-});
-
-
+  
+//load the stored data when the page is refreshed
+$("#8 .description").val(localStorage.getItem("8"));
+$("#9 .description").val(localStorage.getItem("9"));
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#13 .description").val(localStorage.getItem("13"));
+$("#14 .description").val(localStorage.getItem("14"));
+$("#15 .description").val(localStorage.getItem("15"));
+$("#16 .description").val(localStorage.getItem("16"));
+$("#17 .description").val(localStorage.getItem("17"));
 
   //save button will saves the user input in the time box into local storage.
-  
+  console.log("the browser has finished rendering all the elements in the html.");
   $(".saveBtn").on("click", function() {
       let scheduleText = $(this).siblings(".description").val();
       let correctTime = $(this).parent().attr("id")
       localStorage.setItem(correctTime, scheduleText)
   })
-
+});
   let currentTime = dayjs().hour();
   //console.log(currentTime)
 
